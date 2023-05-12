d2b7655c18f611c675279184700daeb1ea18e6eb
OgutuJoseph <onyango.joseph@yahoo.com>
2023-05-12 11:33:29 +0300

    Typescript Data Types and Functions

--- datatypes.js
+++ datatypes.js
@@ -1,3 +1,15 @@
 "use strict";
-let lname = 'John';
-// lname = 10;
+// String  data type
+let lname;
+lname = 'Joseph';
+let newname = lname.toUpperCase();
+console.log('new name: ', newname);
+// Number data type
+let age;
+age = 25;
+let newage = '25';
+let result = parseInt(newage);
+console.log('new age result: ', result);
+// Boolean data type
+let isValid = false;
+console.log('first isValid result: ', isValid);
